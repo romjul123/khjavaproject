5970772669404a55830128b47bf26f5e8dde9739
민 <@192.168.10.104>
2022-11-27 21:37:45 +0900

    add

--- src/com/kh/practice/condition/Practice_If1.java
+++ src/com/kh/practice/condition/Practice_If1.java
@@ -1,8 +1,23 @@
 package com.kh.practice.condition;
 
+import java.util.Scanner;
+
 public class Practice_If1 {
 	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 : ");
+		int num = sc.nextInt();
+		String result = "";
+		if (num == 0) {
+			result = "0이다";
+		} else if (num > 0) {
+			result = "양수다";
+		} else {
+			result = "음수다";
+		}
+		System.out.println(result);
 		
+				
 	}
 
 }
