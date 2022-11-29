b1a1efd60f4307a5755bcf15ff12f53f6a8968be
user1 <user1@DESKTOP-MPUKNTA>
2022-11-29 17:30:51 +0900

    add : run file add
    run file 추가 업로드

---
+++ src/com/kh/day05/run/Run.java
@@ -0,0 +1,17 @@
+package com.kh.day05.run;
+
+import com.kh.day05.exercise.Exercise_Array;
+import com.kh.day05.exercise.Exercise_For;
+import com.kh.day05.exercise.Exercise_While;
+
+public class Run {
+	public static void main(String [] args) {
+		//Exercise_For exFor = new Exercise_For();
+		//exFor.exercise4();
+		//Exercise_While exWhile = new Exercise_While();
+		//exWhile.exercise3();
+		Exercise_Array exArr = new Exercise_Array();
+		exArr.exercise5();
+	}
+
+}
